ea8c679ed48912353caccc930da40cadb9eb376c
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-11-11 12:07:22 +0100

    controller input parsing

--- controller.py
+++ controller.py
@@ -4,7 +4,46 @@ from view import *
 from modell import *
 
 
-res = swapCurrency(100, "USD", "EUR", True)
+
+def calculateValueInNewCurrency(value, fromCurrency, toCurrency, live):
+  if isinstance(str, live):
+    via = live
+  elif isinstance(bool, live):
+    if live:
+      via = "api"
+    else: 
+      via = "offline"
+  else:
+    via = "api"
+
+
+  if isinstance(toCurrency, str):
+    toCurrency = ",".split(toCurrency)
+  else:
+    if not isinstance(toCurrency, list):
+      toCurrency = [str(toCurrency)]
+    else:
+      for i in range(len(toCurrency)):
+        toCurrency[i] = str(toCurrency[i])
+
+  
+  for i in range(len(toCurrency)):
+    toCurrency[i] = upper(toCurrency[i])
+
+  try:
+    res = swapCurrency(value, fromCurrency, toCurrency, via)  
+    return {
+      "ok": True,
+      "res": res
+    }
+  except Exception as e:
+    return {
+      "ok": False,
+      "msg": e.args
+    }
+  
+  
+
 
 
 
@@ -12,6 +51,5 @@ res = swapCurrency(100, "USD", "EUR", True)
 app = QtWidgets.QApplication(sys.argv)
 
 window = Window()
-window.showResult(res)
 window.show()
 sys.exit(app.exec_())
